f30397233b2c96d880b4858add9097629d785def
Abhijeet <Abhijeet@datametica.com>
2014-05-29 10:58:50 +0530

    added project for chaptor 2

---
+++ hdg-ch04-hadoop-io/src/main/java/academy/kabab/hdg/ch04/hadoop/io/App.java
@@ -0,0 +1,13 @@
+package academy.kabab.hdg.ch04.hadoop.io;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
