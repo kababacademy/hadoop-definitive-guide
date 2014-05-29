aa68eb6f1997befd9be3b868b2cd748d91740312
Abhijeet <Abhijeet@datametica.com>
2014-05-29 11:08:01 +0530

    added hadoop dependencies

--- hdg-ch04-hadoop-io/src/main/java/academy/kabab/hdg/ch04/hadoop/io/App.java
+++ hdg-ch04-hadoop-io/src/main/java/academy/kabab/hdg/ch04/hadoop/io/App.java
@@ -1,13 +1,6 @@
 package academy.kabab.hdg.ch04.hadoop.io;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+	}
 }
